14c245bf9ffe1faa99df356ed9aa7ae6c16db02b
jeremy koundour <j.koundour@gmail.com>
2023-07-09 16:42:12 +0200

    mise en place header et navigation

--- front/script/header.js
+++ front/script/header.js
@@ -1,14 +1,8 @@
 const navLink = document.querySelectorAll('.nav-link');
 const windowPathname = window.location.pathname;
 
-console.log(navLink);
-console.log(window.location.pathname);
-
 navLink.forEach((navLink) => {
   const navLinkPathname = new URL(navLink.href).pathname;
-  console.log(navLinkPathname);
-  console.log(windowPathname);
-
   if (windowPathname === navLinkPathname) {
     navLink.classList.add('actives');
   }
