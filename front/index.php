83104d78a6f0e05d458aa792e62ee618012d2b2d
jeremy koundour <j.koundour@gmail.com>
2023-07-10 16:31:04 +0200

    création du composant galerie des services

--- front/index.php
+++ front/index.php
@@ -1,12 +1,15 @@
 
 <link href="../front/css/header.css" rel="stylesheet" />
 <link href="../front/css/footer.css" rel="stylesheet" />
+<link href="../front/css/galleryServices.css" rel="stylesheet" />
 
 
 <?php
 require './components/header.html';
 ?>
-
+<?php
+require './components/galleryServices.html';
+?>
 <?php
 require './components/footer.html';
 ?>
