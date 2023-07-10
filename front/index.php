c594b3ac1dee8ea9c1548a483ff0cc83f37a8846
jeremy koundour <j.koundour@gmail.com>
2023-07-10 21:07:58 +0200

    correction bug affichage

--- front/index.php
+++ front/index.php
@@ -7,9 +7,8 @@
     href="https://getbootstrap.com/docs/5.3/assets/css/docs.css"
     rel="stylesheet"
   />
-<link href="../front/css/header.css" rel="stylesheet" />
-<link href="../front/css/footer.css" rel="stylesheet" />
-<link href="../front/css/galleryServices.css" rel="stylesheet" />
+  <link href="./css/galleryServices.css" rel="stylesheet" />
+  <link href="./css/footer.css" rel="stylesheet" />
 
 <?php
 echo "<div class='container_fluid d-flex flex-column'>";
@@ -28,5 +27,6 @@ echo "<div class='container_fluid d-flex flex-column'>";
 echo "</div>";
 ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../front/script/header.js"></script>
+  <script src="../front/script/header.js"></script>
+
 <script src="../front/script/cardsServices.js"></script>
--- front/script/header.js
+++ front/script/header.js
@@ -1,8 +1,13 @@
-const navLink = document.querySelectorAll('.nav-link'); // lien de navigation du header
-const windowPathname = window.location.pathname; // localisation de la page
+// lien de navigation du header
+const navLink = document.querySelectorAll('.headerNavLink');
+// localisation de la page
+const windowPathname = window.location.pathname;
 
 navLink.forEach((navLink) => {
   //check sur les liens pour comparer la localisation
+  console.log('windowPathname', windowPathname);
+  console.log('navLink', navLink);
+  console.log('new URL(navLink.href).pathname', new URL(navLink.href).pathname);
   const navLinkPathname = new URL(navLink.href).pathname;
   if (windowPathname === navLinkPathname) {
     navLink.classList.add('actives'); // mises en place de la class actives
