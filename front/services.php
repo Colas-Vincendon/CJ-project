6432c12f44f0b3a401ff4cec0b06660a4ba08ff4
jeremy koundour <j.koundour@gmail.com>
2023-07-11 16:16:38 +0200

    mise en place page services

--- front/services.php
+++ front/services.php
@@ -1,3 +1,40 @@
+
+ 
+  <link href="../front/css/header.css" rel="stylesheet" />
+  <link href="../front/css/footer.css" rel="stylesheet" />
+  <link href="../front/css/galleryServices.css" rel="stylesheet" />
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+  />
+  <link
+    href="https://getbootstrap.com/docs/5.3/assets/css/docs.css"
+    rel="stylesheet"
+  />
+
+<?php
+echo "<div class='container_fluid d-flex flex-column'>";
+    echo "<div class='container_fluid'>";
+    echo "<div style='display:block'>";
+    require './components/header.html';
+    echo "</div>";
+
+    echo "<div style='display:block'>";
+    require './components/galleryServices.html';
+    echo "</div>";
+    echo "<div style='display:block'>";
+    require './components/footer.html';
+    echo "</div>";
+    echo "</div>";
+echo "</div>";
+?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../front/script/header.js"></script>  
+<script src="../front/script/cardsServices.js"></script>
+
+</body>
+<!-- ********************************************************************************************************* -->
+
 <?php
 
 require './components/header.html';
