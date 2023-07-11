52202a9de9762389a33852c6e4d1ec42a45896ef
jeremy koundour <j.koundour@gmail.com>
2023-07-11 15:31:39 +0200

    suppression des console

--- front/script/header.js
+++ front/script/header.js
@@ -5,9 +5,6 @@ const windowPathname = window.location.pathname;
 
 navLink.forEach((navLink) => {
   //check sur les liens pour comparer la localisation
-  console.log('windowPathname', windowPathname);
-  console.log('navLink', navLink);
-  console.log('new URL(navLink.href).pathname', new URL(navLink.href).pathname);
   const navLinkPathname = new URL(navLink.href).pathname;
   if (windowPathname === navLinkPathname) {
     navLink.classList.add('actives'); // mises en place de la class actives
