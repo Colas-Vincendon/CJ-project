54f8d256d7cc1433e3d5a8b844f607aa0b5c59b5
Colas <colasvincendon@gmail.com>
2023-07-13 12:27:30 +0200

    modified devis.php and added politic.php

--- front/devis.php
+++ front/devis.php
@@ -2,126 +2,13 @@
 <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
 <link href="../front/css/header.css" rel="stylesheet" />
 <link href="../front/css/footer.css" rel="stylesheet" />
-<link href="../front/css/envoiEmail.css" rel="stylesheet" />
+<link href="../front/css/devis.css" rel="stylesheet" />
 
 <body>
 
     <?php
     require_once '../front/components/header.html';
-    ?>
-
-    <div class="text-center border">
-        <div class="py-5 d-flex justify-content-center align-items-center"
-            style="background-color: #f1f1f1; color: #119da4;">
-            <h2 class>Devis sur mesure pour vos projets de plomberie et chauffage</h2><br>
-        </div>
-
-        <div style="background-image: url('../Medias/pics_popCard/hydraulic-design.jpg');
-  background-size: cover;
-  background-position: center;">
-
-            <div class="row">
-                <div class="col-md-6" style="opacity: 0;">
-                </div>
-                <div class="col-md-6">
-                    <div class="py-4 px-5" style="background-color: #119da4; color: #ffffff; box-shadow: -14px 20px 33px -8px rgba(0,0,0,0.51);
--webkit-box-shadow: -14px 20px 33px -8px rgba(0,0,0,0.51);
--moz-box-shadow: -14px 20px 33px -8px rgba(0,0,0,0.51);">
-                        <p class="fs-5">Nous comprenons que chaque projet de plomberie et de chauffage est unique, c'est
-                            pourquoi nous nous
-                            engageons à fournir des devis sur mesure pour répondre précisément à vos besoins. Nous
-                            croyons
-                            fermement
-                            qu'il est essentiel de visiter votre domicile ou votre entreprise afin d'évaluer
-                            correctement la
-                            situation et de vous fournir une estimation précise.
-                            <br>
-                            <br>
-                            Lorsque vous nous contactez pour discuter de votre projet, nous fixerons un rendez-vous
-                            pour une visite
-                            sur place. Notre équipe de professionnels expérimentés se rendra à votre emplacement pour
-                            évaluer
-                            l'étendue des travaux et discuter de vos attentes et de vos exigences spécifiques. Nous
-                            prendrons le
-                            temps d'écouter attentivement vos besoins et de répondre à toutes vos questions concernant
-                            votre
-                            projet.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="py-4 px-5" style="background-color: #119da4; color: #ffffff; box-shadow: 1px 0px 33px 12px rgba(0,0,0,0.51);
--webkit-box-shadow: 1px 0px 33px 12px rgba(0,0,0,0.51);
--moz-box-shadow: 1px 0px 33px 12px rgba(0,0,0,0.51);">
-                        <p class="fs-5">Une fois que nous aurons collecté toutes les informations nécessaires et évalué
-                            les
-                            aspects techniques,
-                            nous travaillerons sur un devis personnalisé spécialement conçu pour votre projet. Notre
-                            équipe
-                            prendra
-                            en compte tous les détails importants, tels que les matériaux, les équipements, les délais
-                            et
-                            les coûts,
-                            afin de vous fournir une proposition complète et transparente.
-                            <br>
-                            <br>
-                            Nous croyons en une approche honnête et transparente dans notre processus de devis.
-                            Nous veillons à ce
-                            que vous compreniez parfaitement les détails de notre proposition, y compris les coûts
-                            associés
-                            et les
-                            travaux prévus. Nous serons également disponibles pour répondre à toutes vos questions
-                            supplémentaires
-                            et apporter des ajustements si nécessaire.
-                        </p><br>
-                    </div>
-                </div>
-                <div class="col-md-6" style="opacity: 0;">
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col-md-6" style="opacity: 0;">
-                </div>
-                <div class="col-md-6">
-                    <div class="py-4 px-5" style="background-color: #119da4; color: #ffffff; box-shadow: -19px -18px 33px -8px rgba(0,0,0,0.51);
--webkit-box-shadow: -19px -18px 33px -8px rgba(0,0,0,0.51);
--moz-box-shadow: -19px -18px 33px -8px rgba(0,0,0,0.51);">
-                        <p class="fs-5">Notre objectif est de vous offrir un service de plomberie et de chauffage de
-                            qualité
-                            supérieure, en
-                            personnalisant chaque devis en fonction de vos besoins spécifiques. Nous croyons que chaque
-                            projet est
-                            unique et mérite une approche individualisée pour garantir votre satisfaction totale.
-                            <br>
-                            <br>
-                            Contactez-nous
-                            dès aujourd'hui pour planifier une visite sur place et obtenir un devis
-                            sur mesure pour
-                            votre projet de plomberie et de chauffage. Nous sommes impatients de travailler avec vous et
-                            de
-                            réaliser
-                            vos projets selon vos exigences les plus élevées.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row py-5" text-center>
-                <h1 class="pb-4" style="color:#119da4">Réalisons votre projet ensemble !</h1>
-                <a href="contact.php">
-                    <button class="btn btn-primary">Contactez-nous</button>
-                </a>
-            </div>
-        </div>
-
-    </div>
-
-    <?php
+    require_once '../front/components/devis.html';
     require_once '../front/components/footer.html';
     ?>
 
