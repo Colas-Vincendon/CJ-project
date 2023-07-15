c7bb52060cb8986763f39cb0aff76f9f2b7a61e3
Colas <colasvincendon@gmail.com>
2023-07-15 21:40:43 +0200

    changed meta for responsive

--- front/successEmail.php
+++ front/successEmail.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
 <link href="../front/css/header.css" rel="stylesheet" />
