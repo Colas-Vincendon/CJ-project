5d58d9a33a6fe686665635521648788d0678d9a2
Colas <colasvincendon@gmail.com>
2023-07-31 22:32:12 +0200

    created composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'colas/rhoneau',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c2829877fe70bc766fb320565342a2ab185ca4f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'colas/rhoneau' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c2829877fe70bc766fb320565342a2ab185ca4f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
